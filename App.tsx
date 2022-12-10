0dca08640cceed6e9ee3d0836733dd1527459b9e
Melk de Sousa <melk.sousap@gmail.com>
2022-12-09 21:01:45 -0300

    📦 - build: install deps, change extension file

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useState } from 'react'
+import { FlatList, SafeAreaView, StatusBar, StyleSheet } from 'react-native'
+import { Nota } from './src/componentes/Nota'
+import NotaEditor from './src/componentes/NotaEditor'
+export default function App() {
+	const [notas, setNotas] = useState([])
+	async function mostraNotas() {
+		const todasChaves = await AsyncStorage.getAllKeys()
+		const todasNotas = await AsyncStorage.multiGet(todasChaves)
+		setNotas(todasNotas)
+		console.log(todasNotas)
+	}
+	return (
+		<SafeAreaView style={estilos.container}>
+			<FlatList
+				data={notas}
+				renderItem={nota => <Nota {...nota} />}
+				keyExtractor={nota => nota[0]}
+			/>
+			<NotaEditor mostraNotas={mostraNotas} />
+			<StatusBar />
+		</SafeAreaView>
+	)
+}
+
+const estilos = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'stretch',
+		justifyContent: 'flex-start'
+	}
+})
